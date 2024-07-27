c1c027df2c825a03201ccbec2f917a9b18ef3ac9
Watermelon-star <paletisarahsuhitha18@gmail.com>
2024-07-27 15:40:18 +0530

    Add files via upload

---
+++ ChowdaryPy.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chowdary.py;
+
+/**
+ *
+ * @author student
+ */
+public class ChowdaryPy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
